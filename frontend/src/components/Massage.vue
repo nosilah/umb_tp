41baeca74e19387c0ace3a74e8bddff1b654a956
nosilah <alnosila.abd@gmail.com>
2021-11-01 16:06:13 +0300

    add env.ex

--- frontend/src/components/Massage.vue
+++ frontend/src/components/Massage.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <div class="col-md-12 mt-n3">
-      <div class="card card-container ">
-        <h2>
-          open  email and click on the link for authorization
-        </h2>
+    <div class="col-md-12 ">
+      <div class="m-1000">
+        <h4>
+          check email box and click on the link for authorization
+        </h4>
       </div>
     </div>
   </div>
